3c87a5ba12e7df39715d8a3a2b1e529478ff30a2
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-13 06:08:07 +0000

    In the making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book.user_id = current_user.id
     if @book.save
-    flash[:notice] = 'You have created book succesfully.'
+    flash[:notice] = 'You have created book successfully.'
     redirect_to book_path(@book.id)
     else
     @user = current_user
